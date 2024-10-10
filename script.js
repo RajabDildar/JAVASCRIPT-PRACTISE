42baf68d79a2dd30448b26bc2c97496b9e77d96a
RajabDildar <rajabdildar4678@gmail.com>
2024-10-10 21:56:34 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("welcome in js series");
+console.log("Rajab Ali Dildar");
+alert("This Is Rajab Ali");
+
+// variable start
+fullname = "myname";
+console.log(fullname);
+// variable ko double ya single quotes ' " mei nahi likhen ge
+age = 24;
+console.log(age);
+console.log(age + 3);
+price = 100;
+console.log(price);
